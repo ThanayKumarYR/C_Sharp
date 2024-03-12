2ba5ac13acc3a8ad71118d4d2c00b0a8469d589f
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-12 11:22:00 +0530

    Read lines

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace ConsoleApp3
             Console.Write("This method will not produce a new line");
             Console.WriteLine("This method will produce a new line");
 
+            //Read Lines
+            Console.WriteLine("Enter the value to the read line which takes string as input !.");
+            Console.ReadLine();
+
         }
     }
 }
