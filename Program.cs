312453fbc2288a54945c40dab2251c98ed63d58c
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-12 11:42:58 +0530

    data types

--- Program.cs
+++ Program.cs
@@ -3,7 +3,35 @@
 namespace ConsoleApp3
 {
     class Program
+
     {
+
+        public void dataTypes() 
+        {
+            byte a = 1;
+            short b = 2;
+            int c = 32000;
+            float d = 4.458945f;
+            double e = 5.562394870948309d;
+            long f = 6092384098420948L;
+            char g = 'A';
+            bool h = true;
+            DateTime l = DateTime.Now;
+            string m = "'I miss you'";
+            object o = new object();
+
+            Console.WriteLine($"{a} is a byte!");
+            Console.WriteLine($"{b} is a short!");
+            Console.WriteLine($"{c} is an integer!");
+            Console.WriteLine($"{d} is a float!");
+            Console.WriteLine($"{e} is a double!");
+            Console.WriteLine($"{f} is a long!");
+            Console.WriteLine($"{g} is a char!");
+            Console.WriteLine($"{h} is a boolean!");
+            Console.WriteLine($"{l} is a Date!");
+            Console.WriteLine($"{m} is an string!");
+            Console.WriteLine($"{o} is an object!");
+        }
         static void Main(string[] args)
         {
             //First program
@@ -26,6 +54,9 @@ namespace ConsoleApp3
              * line
              * comment
              */
+
+            Program obg = new Program();
+            obg.dataTypes();
         }
     }
 }
